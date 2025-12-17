dd81c7e771350d88044847c8e22f91ac76053146
bala-amypo <bala@amypo.in>
2025-12-17 06:29:46 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -2,7 +2,7 @@ package com.example.project.entity;
 
 import jakarta.persistence.*;
 
-@Entity
+@entity
 public class Student{
     @Id
     @GeneratesValue(strategy = GenerationType.IDENTITY)
---
+++ src/main/java/com/example/demo/repository/StudentRepo.java
@@ -0,0 +1,5 @@
+package.com.example.demo.repository;
+import org.springframework.web.bind.annotation.*;
+import
+@repository
+public interface StudentRepo extends JpaRepository{}
